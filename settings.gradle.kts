7bcf7380d80f96aec1ac97e4c0324ef67467f035
ngangavic <ngangavictor10@gmail.com>
2022-05-07 12:06:42 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SMS_my_call"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/ngangavictor/smsmycall/Platform.kt
@@ -0,0 +1,5 @@
+package com.ngangavictor.smsmycall
+
+expect class Platform() {
+    val platform: String
+}
